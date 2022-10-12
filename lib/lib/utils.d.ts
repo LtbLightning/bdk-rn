b7e2d84e919d52a73cb713e6d1e536782f4df4c1
Corey Phillips <corey.lyle.phillips@protonmail.com>
2022-10-12 09:59:51 -0400

    Added @synonymdev/result dependency to package.json. Removed & Replaced previous Response pattern with @synonymdev/result where needed. Updated README.md.

--- lib/lib/utils.d.ts
+++ lib/lib/utils.d.ts
@@ -1,7 +1,2 @@
-import { Response } from './interfaces';
 /** Check if value is exists and not empty */
 export declare const _exists: (value: any) => any;
-/** Return failed response */
-export declare const failure: (data?: any) => Response;
-/** Return success response */
-export declare const success: (data?: string | object | any) => Response;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,4 @@
 import is from 'is_js';
-import { Response } from './interfaces';
 
 /** Check if value is exists and not empty */
 export const _exists = (value: any) => is.existy(value) && is.not.empty(value);
-
-/** Return failed response */
-export const failure = (data: any = ''): Response => ({
-  error: true,
-  data: data.code ? `Code: ${data.code} Message: ${data.message} ` : data,
-});
-
-/** Return success response */
-export const success = (data: string | object | any = ''): Response => ({
-  error: false,
-  data,
-});
