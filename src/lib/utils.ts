417b4dda22285aada0130f7f9116bf4ab27e65b6
Czino <eric@czino.de>
2023-05-16 20:56:21 +0200

    Remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,4 @@
 import { BlockTime, TransactionDetails } from '../classes/Bindings';
-import is from 'is_js';
-
-/** Check if value is exists and not empty */
-export const _exists = (value: any) => is.existy(value) && is.not.empty(value);
 
 type ConfirmationTime = {
   height?: number,
@@ -13,7 +9,7 @@ type Props = {
   received: number,
   sent: number,
   fee?: number
-  confirmationTime: ConfirmationTime
+  confirmationTime?: ConfirmationTime
 }
 
 /** Create TransactionDetails object */
