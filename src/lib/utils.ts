3128544170b8ced9b16f19e5a932f0ff77b1af3c
Czino <eric@czino.de>
2023-05-16 20:52:56 +0200

    Add unit test for createTxDetailsObject

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,20 @@ import is from 'is_js';
 /** Check if value is exists and not empty */
 export const _exists = (value: any) => is.existy(value) && is.not.empty(value);
 
+type ConfirmationTime = {
+  height?: number,
+  timestamp?: number
+}
+type Props = {
+  txid: string,
+  received: number,
+  sent: number,
+  fee?: number
+  confirmationTime: ConfirmationTime
+}
+
 /** Create TransactionDetails object */
-export const createTxDetailsObject = (item: any): TransactionDetails => {
+export const createTxDetailsObject = (item: Props): TransactionDetails => {
   return new TransactionDetails(
     item.txid,
     item.received,
