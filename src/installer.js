d18321f9d447d12b751627dc4712eb3a51ea3d8d
viva <viva@peachbitcoin.com>
2024-10-31 14:17:08 +0100

    Security Update

--- src/installer.js
+++ src/installer.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const axios = require('axios');
 const fs = require('fs');
 var AdmZip = require('adm-zip');
 
@@ -7,14 +7,25 @@ var fileUrl = 'https://github.com/bitcoindevkit/bdk-swift/releases/download/0.30
 const src = 'ios/bdkFFI.xcframework.zip';
 const target = 'ios/';
 
-try {
-  request(fileUrl)
-    .pipe(fs.createWriteStream(src))
-    .on('close', function () {
+async function downloadAndExtract() {
+  try {
+    const response = await axios({
+      url: fileUrl,
+      method: 'GET',
+      responseType: 'stream',
+    });
+
+    const dest = fs.createWriteStream(src);
+    response.data.pipe(dest);
+
+    dest.on('finish', () => {
       var zip = new AdmZip(src);
       zip.extractAllTo(target, true);
       fs.unlinkSync(src);
     });
-} catch (err) {
-  console.log(err);
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+downloadAndExtract();
