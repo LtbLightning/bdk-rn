d25dafa9cb93954a315504df2caf4977c7791424
BitcoinZavior <BitcoinZavior@GMail.Com>
2025-01-21 18:03:00 -0500

    Updated Transactions Tests

--- __tests__/classes/Transaction.spec.ts
+++ __tests__/classes/Transaction.spec.ts
@@ -4,96 +4,114 @@ import { mockTxIn, mockTxOut } from '../mockData';
 import { mockBdkRnModule } from '../setup';
 
 describe('Transaction', () => {
+  const id = 'transactionId';
+  const txId = 'txId123';
   const rawTx = [1, 2, 3];
-  const id = '5BD8F815-408D-4D5D-B917-C3DE0986EC7F';
-  const txId = '271ee42f7c527a9215a058be427453f89aad7a8835711659c6ee8ba8953f7651';
   let transaction: Transaction;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     mockBdkRnModule.createTransaction.mockResolvedValue(id);
     transaction = await Transaction.create(rawTx);
   });
 
-  it('id is stored in class', async () => {
-    expect(transaction.id).toBe(id);
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('serializes transcation', async () => {
-    mockBdkRnModule.serializeTransaction.mockResolvedValueOnce(rawTx);
-    const serialized = await transaction.serialize();
-    expect(mockBdkRnModule.serializeTransaction).toHaveBeenCalledWith(transaction.id);
-    expect(serialized).toBe(rawTx);
+  describe('static methods', () => {
+    it('creates transaction from raw bytes', async () => {
+      expect(transaction.id).toBe(id);
+      expect(mockBdkRnModule.createTransaction).toHaveBeenCalledWith(rawTx);
+    });
+
+    it('creates transaction from extracted data', () => {
+      const txFromData = Transaction.fromData(id);
+      expect(txFromData.id).toBe(id);
+    });
   });
 
-  it('verify txid()', async () => {
-    mockBdkRnModule.transactionTxid.mockResolvedValueOnce(txId);
-    const res = await transaction.txid();
-    expect(res).toBe(txId);
-    expect(mockBdkRnModule.transactionTxid).toHaveBeenCalledWith(transaction.id);
+  describe('serialization', () => {
+    it('serializes transaction', async () => {
+      mockBdkRnModule.serializeTransaction.mockResolvedValueOnce(rawTx);
+      const serialized = await transaction.serialize();
+      expect(mockBdkRnModule.serializeTransaction).toHaveBeenCalledWith(transaction.id);
+      expect(serialized).toEqual(rawTx);
+    });
   });
 
-  it('verify weight()', async () => {
-    mockBdkRnModule.txWeight.mockResolvedValueOnce(616);
-    const res = await transaction.weight();
-    expect(res).toBe(616);
-    expect(mockBdkRnModule.txWeight).toHaveBeenCalledWith(transaction.id);
-  });
-  it('verify size()', async () => {
-    mockBdkRnModule.txSize.mockResolvedValueOnce(154);
-    const res = await transaction.size();
-    expect(res).toBe(154);
-    expect(mockBdkRnModule.txSize).toHaveBeenCalledWith(transaction.id);
+  describe('transaction properties', () => {
+    it('retrieves txid', async () => {
+      mockBdkRnModule.transactionTxid.mockResolvedValueOnce(txId);
+      const res = await transaction.txid();
+      expect(res).toBe(txId);
+      expect(mockBdkRnModule.transactionTxid).toHaveBeenCalledWith(transaction.id);
+    });
+
+    it('retrieves weight', async () => {
+      const weight = 100;
+      mockBdkRnModule.txWeight.mockResolvedValueOnce(weight);
+      const res = await transaction.weight();
+      expect(res).toBe(weight);
+      expect(mockBdkRnModule.txWeight).toHaveBeenCalledWith(transaction.id);
+    });
+
+    it('retrieves size', async () => {
+      const size = 200;
+      mockBdkRnModule.txSize.mockResolvedValueOnce(size);
+      const res = await transaction.size();
+      expect(res).toBe(size);
+      expect(mockBdkRnModule.txSize).toHaveBeenCalledWith(transaction.id);
+    });
+
+    it('retrieves vsize', async () => {
+      const vsize = 300;
+      mockBdkRnModule.txVsize.mockResolvedValueOnce(vsize);
+      const res = await transaction.vsize();
+      expect(res).toBe(vsize);
+      expect(mockBdkRnModule.txVsize).toHaveBeenCalledWith(transaction.id);
+    });
+
+    it('retrieves version', async () => {
+      const version = 2;
+      mockBdkRnModule.txVersion.mockResolvedValueOnce(version);
+      const res = await transaction.version();
+      expect(res).toBe(version);
+      expect(mockBdkRnModule.txVersion).toHaveBeenCalledWith(transaction.id);
+    });
+
+    it('retrieves lockTime', async () => {
+      const lockTime = 500000;
+      mockBdkRnModule.txLockTime.mockResolvedValueOnce(lockTime);
+      const res = await transaction.lockTime();
+      expect(res).toBe(lockTime);
+      expect(mockBdkRnModule.txLockTime).toHaveBeenCalledWith(transaction.id);
+    });
   });
 
-  it('verify vsize()', async () => {
-    mockBdkRnModule.txVsize.mockResolvedValueOnce(154);
-    const res = await transaction.vsize();
-    expect(res).toBe(154);
-    expect(mockBdkRnModule.txVsize).toHaveBeenCalledWith(transaction.id);
+  describe('transaction status', () => {
+    it('checks if transaction is coinbase', async () => {
+      mockBdkRnModule.txIsCoinBase.mockResolvedValueOnce(false);
+      const res = await transaction.isCoinBase();
+      expect(res).toBe(false);
+      expect(mockBdkRnModule.txIsCoinBase).toHaveBeenCalledWith(transaction.id);
+    });
+
+    it('checks if transaction is explicitly RBF', async () => {
+      mockBdkRnModule.txIsExplicitlyRbf.mockResolvedValueOnce(true);
+      const res = await transaction.isExplicitlyRbf();
+      expect(res).toBe(true);
+      expect(mockBdkRnModule.txIsExplicitlyRbf).toHaveBeenCalledWith(transaction.id);
+    });
+
+    it('checks if locktime is enabled', async () => {
+      mockBdkRnModule.txIsLockTimeEnabled.mockResolvedValueOnce(true);
+      const res = await transaction.isLockTimeEnabled();
+      expect(res).toBe(true);
+      expect(mockBdkRnModule.txIsLockTimeEnabled).toHaveBeenCalledWith(transaction.id);
+    });
   });
 
-  it('verify isCoinBase()', async () => {
-    mockBdkRnModule.txIsCoinBase.mockResolvedValueOnce(false);
-    const res = await transaction.isCoinBase();
-    expect(res).toBe(false);
-    expect(mockBdkRnModule.txIsCoinBase).toHaveBeenCalledWith(transaction.id);
-  });
-
-  it('verify isExplicitlyRbf()', async () => {
-    mockBdkRnModule.txIsExplicitlyRbf.mockResolvedValueOnce(false);
-    const res = await transaction.isExplicitlyRbf();
-    expect(res).toBe(false);
-    expect(mockBdkRnModule.txIsExplicitlyRbf).toHaveBeenCalledWith(transaction.id);
-  });
-
-  it('verify txIsLockTimeEnabled()', async () => {
-    mockBdkRnModule.txIsLockTimeEnabled.mockResolvedValueOnce(true);
-    const res = await transaction.isLockTimeEnabled();
-    expect(res).toBe(true);
-    expect(mockBdkRnModule.txIsLockTimeEnabled).toHaveBeenCalledWith(transaction.id);
-  });
-  it('verify version()', async () => {
-    mockBdkRnModule.txVersion.mockResolvedValueOnce(1);
-    const res = await transaction.version();
-    expect(res).toBe(1);
-    expect(mockBdkRnModule.txVersion).toHaveBeenCalledWith(transaction.id);
-  });
-
-  it('verify lockTime()', async () => {
-    mockBdkRnModule.txLockTime.mockResolvedValueOnce(1);
-    const res = await transaction.lockTime();
-    expect(res).toBe(1);
-    expect(mockBdkRnModule.txLockTime).toHaveBeenCalledWith(transaction.id);
-  });
-
-  it('verify input()', async () => {
-    mockBdkRnModule.txInput.mockResolvedValueOnce([mockTxIn]);
-    const res = await transaction.input();
-    expect(res).toStrictEqual([createTxIn(mockTxIn)]);
-    expect(mockBdkRnModule.txInput).toHaveBeenCalledWith(transaction.id);
-  });
-
-  it('verify output()', async () => {
+  it('retrieves transaction outputs', async () => {
     mockBdkRnModule.txOutput.mockResolvedValueOnce([mockTxOut]);
     const res = await transaction.output();
     expect(res).toStrictEqual([createTxOut(mockTxOut)]);
